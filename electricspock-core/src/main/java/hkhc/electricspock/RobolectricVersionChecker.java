b331499861265fdb6cc40e6a1ab50abd5ab67796
Nikolas Thitu <nikolas@hauz.io>
2021-12-23 21:21:59 -0500

    bumping up to robolectric 4.7.3

--- electricspock-core/src/main/java/hkhc/electricspock/RobolectricVersionChecker.java
+++ electricspock-core/src/main/java/hkhc/electricspock/RobolectricVersionChecker.java
@@ -29,7 +29,7 @@ public class RobolectricVersionChecker {
   private String versionKey = "robolectric.version";
 
   // *** update this for version upgrade
-  private String[] acceptedVersions = new String[]{"4,0", "4.1", "4.2", "4.3", "4.4", "4.6"};
+  private String[] acceptedVersions = new String[]{"4.7"};
 
   public RobolectricVersionChecker() {
   }
